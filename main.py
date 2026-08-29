87eb89b5c8fecb1c71d324cd90eb86d5c9bd069e
R1-universe <r.pahlavan2009@gmail.com>
2026-08-30 02:17:25 +0330

    Add Search Operations

--- main.py
+++ main.py
@@ -13,20 +13,31 @@ if __name__ == "__main__":
     for mobile in PHONE_NUMBER:
         User(choice(FIRST_NAME), choice(LAST_NAME), mobile)
 
-    reg1 = Region("Faramarz")
+    faramarz = Region("Faramarz")
+    vakilabad = Region("VakilAbad")
     
     #Advertisement Object
     aps1 = ApartmentSell(
         has_elevator=True, has_parking=True, floor=2,
         user=User.object_list[0], area=3, room_count=4,
-        built_year=1400, region=reg1, address="Faramarz 1...",
+        built_year=1400, region=faramarz, address="Faramarz 1...",
         price_per_meter=50, discountable=True, convertable=True
     )
 
     Hs1 = HouseSell(
         has_yard=True, floor_count=2,
         user=User.object_list[2], area=90, room_count=3,
-        built_year=1400, region=reg1, address="Faramarz 22...",
+        built_year=1400, region=vakilabad, address="VakilAbad 22...",
         price_per_meter=100, discountable=True, convertable=True
     )
-    Hs1.show_detail()
+    #Hs1.show_detail()
+
+    Hs2 = HouseSell(
+        has_yard=True, floor_count=2,
+        user=User.object_list[2], area=90, room_count=3,
+        built_year=1400, region=faramarz, address="Faramarz 22...",
+        price_per_meter=100, discountable=True, convertable=True
+    )
+
+    res = HouseSell.manager.search(region=faramarz)
+    print(res)
---
+++ manager.py
@@ -0,0 +1,11 @@
+class Manager:
+    def __init__(self, _class=None):
+        self._class = _class
+
+    def search(self, **kwargs):
+        result = list()
+        for key, value in kwargs.items():
+            for obj in self._class.object_list:
+                if hasattr(obj, key) and getattr(obj, key) == value:
+                    result.append(obj)
+        return result
